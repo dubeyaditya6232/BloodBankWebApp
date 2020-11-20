91e58ea015e2ff8aeaf3aec84a5b6d4d72ad908b
Aditya Kumar Dubey <dubeyaditya6232@gmail.com>
2020-11-20 13:37:40 +0530

    added Admin login

--- login.php
+++ login.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-if(  isset($_SESSION['username']) )
+error_reporting(0);
+if($_SESSION['Admin']==='YES')
+header("location:admin/admin.php");
+else if(  isset($_SESSION['username']) )
 {
   header("location:login-page.php");
   die();
@@ -33,6 +36,39 @@ if($db)
       }
   }
 }
+if(isset($_POST['Adminlogin_btn']))
+{
+  $username1=mysqli_real_escape_string($db,$_POST['username']);
+  $password1=mysqli_real_escape_string($db,$_POST['password']);
+  $password1=md5($password1);
+  $sql2="SELECT * FROM users WHERE  Admin='YES'";
+  $result2=$db->query($sql2);
+  if($result2->num_rows<1)
+  {
+    echo '<script type="text/javascript">alert("You are not Authenticated");
+    window.location.href = "login.php";
+   </script>';
+  }
+  else
+  {
+    $sql1="SELECT * FROM users WHERE  username='$username1' AND password='$password1'";
+  $result1=mysqli_query($db,$sql1);
+  if( mysqli_num_rows($result1)>=1)
+  {
+    $_SESSION['username']=$username1;
+    $_SESSION['Admin']='YES';
+    echo '<script type="text/javascript">alert("loggedIN as Admin");
+    window.location.href = "admin/admin.php";
+   </script>';
+  }
+  else
+  {
+    echo '<script type="text/javascript">alert("User Name Password Incorrect");
+    window.location.href = "login.php";
+   </script>';
+  }
+}
+}
 ?>
 
 <!DOCTYPE html>
@@ -46,16 +82,15 @@ if($db)
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="style.css">
   <style>
-  /*#error_msg
-{
-    width:50%;
-    margin:5px auto;
-    height:30px;
-    border:1px solid #FF0000;
-   color:#FF0000;
-   text-align:center;
-   padding-top:10px;
-}*/
+.nav-tabs > li {
+    float:none;
+    display:inline-block;
+    zoom:1;
+}
+
+.nav-tabs {
+    text-align:center;
+}
 table { 
  border-collapse: collapse; 
   }
@@ -104,33 +139,64 @@ body{
 <?php
     if(isset($_SESSION['message']))
     {
-         echo "<div id='error_msg'>".$_SESSION['message']."</div>";
+         echo "<div class='text-center'>".$_SESSION['message']."</div>";
          unset($_SESSION['message']);
     }
 ?>
 <center>
-<div>
-<form method="post" action="login.php">
-<h1 class="site-title" style="text-align: center; color: Red;">  Log In  </h1>
-  <table>
-     <tr>
-           <td><p><font color="red"><b>Username : </b></font></p></td>
-           <td><input type="text" name="username" class="textInput" required></td>
-     </tr>
-      <tr>
-           <td><p><font color="red"><b>Password : </b></font></p></td>
-           <td><input type="password" name="password" class="textInput" required></td>
-     </tr> 
-</table>
-<br>
-<input type="Submit" name="login_btn" class="Log In">
-</form>
-</div>
-</center>
-<br>
-<div class="text-center">Do not have an account? <a href="register.php">Sign up</a></div>
+  <ul class="nav nav-tabs"  role="tablist" >
+    <li class="nav-item">
+      <a class="nav-link active" data-toggle="tab" href="#UserLogin">User Login</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#AdminLogin">Admin Login</a>
+    </li>
+  </ul>
+  <div class="tab-content">
+     <div id="UserLogin" class="container tab-pane  active" role="tabpanel" aria-labelledby="UserLogin-tab">
+        <form method="post" action="login.php">
+           <h1>  User Login  </h1>
+           <table>
+               <tr>
+                   <td><p><font color="red"><b>Username : </b></font></p></td>
+                   <td><input type="text" name="username" class="textInput" required></td>
+               </tr>
+               <tr>
+                   <td><p><font color="red"><b>Password : </b></font></p></td>
+                   <td><input type="password" name="password" class="textInput" required></td>
+               </tr> 
+            </table>
+               <br>
+               <input type="Submit" name="login_btn" class="btn btn-primary">
+        </form>
+              <br>
+              <div>Do not have an account? <a type="submit" href="register.php">Sign up</a></div>
+      </div>
+      <div id="AdminLogin" class="container tab-pane fade" role="tabpanel"aria-labelledby="AdminLogin-tab">
+            <form method="post" action="login.php">
+                 <h1 > Admin Login  </h1>
+                 <table>
+                 <tr>
+                   <td><p><font color="red"><b>Username : </b></font></p></td>
+                   <td><input type="text" name="username" class="textInput" required></td>
+                  </tr>
+                  <tr>
+                   <td><p><font color="red"><b>Password : </b></font></p></td>
+                   <td><input type="password" name="password" class="textInput" required></td>
+                  </tr> 
+                   </table>
+                   <br>
+                   <input type="Submit" name="Adminlogin_btn" class="btn btn-primary">
+            </form>
+              <br>
+              
+       </div>
+  </div>
+  </center>
+
+  
+
 </main>
-</div>
 
 </body>
 </html>
