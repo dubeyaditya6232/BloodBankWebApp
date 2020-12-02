b20ca86551d5f06dfc4db124aef7ab610f7ddea2
Ashwin <ashwinpuntambekar@gmail.com>
2020-12-02 13:20:32 +0530

    Adding CSS to forgot password pages

--- enter-otp.php
+++ enter-otp.php
@@ -22,24 +22,106 @@ if (isset($_POST['checkOTP_btn'])) {
       echo '<script>alert("Invalid OTP");</script>';
       }
   }
+if (isset($_POST['back_btn'])) {
+    echo '<script>window.location.href = "forgotpass.php";</script>';
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create New Password</title>
+    <title>Verification</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+    body {
+
+      background-color: #cceabb;
+    }
+
+    /*.background{
+  background-image: url('img/abg3.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  width:100%;
+  height:auto;
+}*/
+      
+    .nav-tabs {
+      text-align: center;
+    }
+
+    #logo1 img {
+      width: 25%;
+
+    }
+
+    table {
+      border-collapse: collapse;
+    }
+
+    th,
+    td {
+      padding: 0.5rem;
+    }
+
+    #UserLogin {
+      width: 35rem;
+      padding: 1rem;;
+    }
+      
+    .tab { 
+      display: inline-block; 
+      margin-left: 40px;
+    }
+      
+    @media only screen and (max-width: 600px) {
+      h2 {
+        font-size: 2rem;
+      }
+
+      #logo1 img {
+        width: 50%;
+      }
+
+      #UserLogin {
+        width: fit-content;
+      }
+    }
+  </style>
 </head>
-<body>
-<form method="POST">
-            <div class="form-group second_box">
-              <span>
+<body class=background>
+  <div id="logo1">
+    <center><img class="img-responsive" src="img/logo.jpg" alt="RAKTIM"></center>
+  </div>
+  <br>
+  <main class="main-content">
+    <center>
+        <div class="tab-content">
+        <div id="UserLogin" class="container tab-pane  active " role="tabpanel" aria-labelledby="EnterOTP-tab">
+            <form method="POST">
+                <h2> Verify Your OTP </h2>
+                <hr style="border: 1px solid black;">
+                <div class="form-group">
+                <span>
+                <font color="red"><b> *Your Username is <?php echo $_SESSION['user'] ?> </b></font>
+                </span>
+                <br><br>    
+                <span>
                 <font color="red"><b> Enter OTP : </b></font>
-              </span><br>
-              <input type="text" name="otp" class="textInput form-control" required>
-              <br>
-              <button  name="checkOTP_btn" class="btn btn-success">Continue</button>
-            </div>
-          </form>
+                </span><br><br>
+                <input type="text" name="otp" class="textInput form-control" style="width:150px;">
+                <br><br>
+                    <button name="back_btn" class="btn btn-success" style="width:90px;">Back</button>
+                    <span class="tab"></span>
+                    <button  name="checkOTP_btn" class="btn btn-success">Continue</button>
+                </div>
+            </form>
+        </div>
+        </div>
+    </center>  
+  </main>
 </body>
 </html>
--- new-password.php
+++ new-password.php
@@ -24,23 +24,96 @@ if (isset($_POST['continue_btn'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Create new Password</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+    body {
+
+      background-color: #cceabb;
+    }
+
+    /*.background{
+  background-image: url('img/abg3.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  width:100%;
+  height:auto;
+}*/
+      
+    .nav-tabs {
+      text-align: center;
+    }
+
+    #logo1 img {
+      width: 25%;
+
+    }
+
+    table {
+      border-collapse: collapse;
+    }
+
+    th,
+    td {
+      padding: 0.5rem;
+    }
+
+    #UserLogin {
+      width: 35rem;
+      padding: 1rem;;
+    }
+      
+    .tab { 
+      display: inline-block; 
+      margin-left: 40px;
+    }
+      
+    @media only screen and (max-width: 600px) {
+      h2 {
+        font-size: 2rem;
+      }
+
+      #logo1 img {
+        width: 50%;
+      }
+
+      #UserLogin {
+        width: fit-content;
+      }
+    }
+  </style>
 </head>
-<body>
-<form method="POST">
-            <div class="form-group third_box">
+<body class=background>
+  <div id="logo1">
+    <center><img class="img-responsive" src="img/logo.jpg" alt="RAKTIM"></center>
+  </div>
+  <br>
+  <main class="main-content">
+    <center>
+        <div class="tab-content">
+        <div id="UserLogin" class="container tab-pane  active " role="tabpanel" aria-labelledby="newPass-tab">
+        <form method="POST">
+            <h2> Create New Password </h2>
+            <hr style="border: 1px solid black;">
+            <div class="form-group">
               <span>
                 <font color="red"><b> Enter New Password : </b></font>
               </span>
-              <input type="password" name="new_pass" class="textInput form-control"  required>
+              <input type="password" name="new_pass" class="textInput form-control" style="width:250px;"  required>
               <br>
               <span>
-                <font color="red"><b> Enter New Password : </b></font>
+                <font color="red"><b> Confirm Password : </b></font>
               </span>
-              <input type="password" name="confirm_pass" class="textInput form-control" required>
-            <br>
+              <input type="password" name="confirm_pass" class="textInput form-control" style="width:250px;" required>
+            <br><br>
             <button name="continue_btn" class="btn btn-success">Continue to Login</button>
             </div>
-          </form>
+        </form>
+        </div>
+        </div>
+    </center>
+  </main>
 </body>
 </html>
